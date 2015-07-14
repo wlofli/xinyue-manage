37cd86365a2b7f0774ce71a964ac194b8a0ab351
wlofli <378476772@qq.com>
2015-07-14 10:32:54 +0800

    ..
    ..

--- src/main/java/com/xinyue/manage/util/WordUtil.java
+++ src/main/java/com/xinyue/manage/util/WordUtil.java
@@ -4,6 +4,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,8 +33,9 @@ public class WordUtil {
 	
 	private Template getTemplate(String templatePath, String templateName) throws IOException{
 		configuration.setClassForTemplateLoading(this.getClass(), templatePath);
-		Template t = configuration.getTemplate(templateName);
+		Template t  = configuration.getTemplate(templateName);
 		t.setEncoding("UTF-8");
+
 		return t;
 		
 	}
@@ -56,11 +58,16 @@ public class WordUtil {
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("code", "20150713");
 		map.put("phone", "18768104912");
+		map.put("status", "新越网审核中");
+		map.put("createdTime", new Date().toString());
+		map.put("contactName", "李先生");
+		map.put("company", "摩科商用设备有限公司");
+		
 		WordUtil handler = new WordUtil();
 		Writer out;
 		try {
-			out = new OutputStreamWriter(new FileOutputStream("D:\\tmp\\test.doc"), "UTF-8");
-			handler.write("E:\\workspace\\xinyue-manage\\src\\main\\resource\\wordtemplete", "orderTemplete.xml", map, out);
+			out = new OutputStreamWriter(new FileOutputStream("D:/tmp/test.doc"), "UTF-8");
+			handler.write("/com/xinyue/manage/util", "order.xml", map, out);
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
